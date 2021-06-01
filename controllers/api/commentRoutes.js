3df2d77b584b31bf695f20e035f97ad235119697
Catherine Smith <catsmith24601@gmail.com>
2021-06-01 15:08:27 -0400

    playing with/fixing commentRoutes.js

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -41,10 +41,15 @@ router.post('/', withAuth, async (req, res) => {
 router.put('/:id', withAuth, async (req, res) =>{
   try{
       const commentData = await Comment.update(
-          { comment_text: req.body.post_name },
-          { returning: true, where: {id: req.params.id }}
+          { content: req.body.content,
+          user_id: req.body.user_id,
+        post_id: req.body.post_id },
+          { where: {id: req.params.id }}
       )
-      res.status(200).json(commentData)
+      if (!commentData) {
+        res.status(404).json({ message: 'No comment found with this id.' });
+      } else {
+      res.status(200).json(commentData)};
   } catch(err){
       res.status(500).json(err);
   }
@@ -54,18 +59,12 @@ router.put('/:id', withAuth, async (req, res) =>{
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const commentData = await Comment.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
+      where: { id: req.params.id },
     });
 
     if (!commentData) {
       res.status(404).json({ message: 'No comment found with this id!' });
-      return;
-    }
-
-    res.status(200).json(commentData);
+    } else {res.status(200).json(commentData)};
   } catch (err) {
     res.status(500).json(err);
   }
