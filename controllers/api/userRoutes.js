85ab94b9989d44a40227baf55f4acb2ccfec9800
Catherine Smith <catsmith24601@gmail.com>
2021-06-01 14:24:23 -0400

    added delete, put, and get routes for userRoutes.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -43,22 +43,29 @@ router.get('/:id', (req, res) => {
 });
 
 
-
+// POST for endpoint /api/users
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
-
+    // const userData = await User.create(req.body);
+    const userData = await User.create ({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+    // to save user info for duration of session
     req.session.save(() => {
       req.session.user_id = userData.id;
+      req.session.username = userData.username;
       req.session.logged_in = true;
 
-      res.status(200).json(userData);
+    res.status(200).json(userData);
     });
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
+// POST - allow user to log in at endpoint /api/login
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
@@ -66,6 +73,7 @@ router.post('/login', async (req, res) => {
     if (!userData) {
       res
         .status(400)
+        // intentionally not specifying if it's the email or password that is incorrect - security purposes
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
@@ -79,8 +87,10 @@ router.post('/login', async (req, res) => {
       return;
     }
 
+    // to save user info for duration of session
     req.session.save(() => {
       req.session.user_id = userData.id;
+      req.session.username = dbUserData.username;
       req.session.logged_in = true;
       
       res.json({ user: userData, message: 'You are now logged in!' });
@@ -91,6 +101,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
+// Allow user to log out - kills session so user must log in again to view
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
@@ -99,6 +110,42 @@ router.post('/logout', (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+// PUT at endpoint /api/users/1
+router.put('/:id', withAuth, (req, res) => {
+  try {
+    const userData = await User.update(req.body, {
+      individualHooks: true,
+      where: {id: req.params.id}
+    })
+    // not an array, right? double check - if needed (!userData[0])
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with this id.' });
+      return;
+    }
+    res.json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  };
+});
+
+
+// DELETE at endpoint /api/users/1
+router.delete('/:id', withAuth, (req, res) => {
+  try {
+    const userData = await User.destroy(req.body, {
+      where: {id: req.params.id}
+    })
+    // not an array, right? double check - if needed (!userData[0])
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with this id.' });
+      return;
+    }
+    res.json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 module.exports = router;
