f20b45a732a83d0b68d70535361a0446f5c9c492
Catherine Smith <catsmith24601@gmail.com>
2021-06-10 16:09:14 -0400

    added plural helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,5 +2,12 @@ module.exports = {
   format_date: (date) => {
     // Format date as MM/DD/YYYY
     return date.toLocaleDateString();
+  },
+  format_plural: (word, number) => {
+    // to say stuff like 2 comments instead of 2 comment
+    if (number !== 1) {
+      return `${word}s`;
+    }
+    return word;
   }
 };
